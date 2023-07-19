1ada13bffcb4fb138ff4a7371bbea2af1f37d766
Imran Naseer M <123950651+imrannaseer0502@users.noreply.github.com>
2023-07-19 14:32:05 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
